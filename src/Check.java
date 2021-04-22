ea8a188e2589d98250d40a1b3b2b9f248e952e30
Maciej Galas <macko-90@tlen.pl>
2021-04-22 14:10:35 +0200

    update check.java

--- src/Check.java
+++ src/Check.java
@@ -1,7 +1,23 @@
 public class Check {
 
     public boolean sprawdz(int tab[][], int sum2) {
+        int[][] tabl = new int[tab.length][tab.length];
+
+        for (int i = 0; i < tab.length; i++) {
+            for (int j = 0; j < tab.length; j++) {
+                tabl[j][tabl.length - 1 - i] = tab[i][j];
+            }
+        }
+
+        if (checkLine(tab, sum2) || checkLine(tabl, sum2))
+            return true;
+
+        return false;
+    }
+
+    public boolean checkLine(int tab[][], int sum2) {
         int sum = 0;
+
         for (int i = 0; i < tab.length; i++) {
             for (int j = 0; j < tab.length; j++) {
                 if (tab[i][j] == sum2 / 5)
@@ -11,28 +27,6 @@ public class Check {
                 if (sum == sum2)
                     return true;
             }
-        }
-
-        sum = 0;
-        for (int i = 0; i < tab.length; i++) {
-            for (int j = 0; j < tab.length; j++) {
-                if (tab[j][i] == sum2 / 5)
-                    sum = sum + tab[j][i];
-                else
-                    sum = 0;
-                if (sum == sum2)
-                    return true;
-            }
-        }
-
-        sum = 0;
-        for (int i = 0; i < tab.length; i++) {  //przekatna
-            if (tab[i][i] == sum2 / 5)
-                sum = sum + tab[i][i];
-            else
-                sum = 0;
-            if (sum == sum2)
-                return true;
         }
 
         sum = 0;
@@ -63,45 +57,10 @@ public class Check {
             m--;
         }
 
-        int[][] tabl = new int[tab.length][tab.length];
-        for (int i = 0; i <tab.length; i++){
-            for(int j = 0; j <tab.length; j++){
-                tabl[j][tabl.length-1-i] = tab[i][j];
-            }
-        }
-
         sum = 0;
-        int x = tabl.length - 1;
-        for (int i = 1; i < tabl.length; i++) {
-            for (int j = 0; j < x; j++) {
-                if (tabl[j][i + j] == sum2 / 5)
-                    sum = sum + tabl[j][i + j];
-                else
-                    sum = 0;
-                if (sum == sum2)
-                    return true;
-            }
-            x--;
-        }
-
-        sum = 0;
-        int y = tabl.length - 1;
-        for (int i = 1; i < tabl.length; i++) {
-            for (int j = 0; j < y; j++) {
-                if (tabl[i + j][j] == sum2 / 5)
-                    sum = sum + tabl[i + j][j];
-                else
-                    sum = 0;
-                if (sum == sum2)
-                    return true;
-            }
-            y--;
-        }
-
-        sum = 0;
-        for (int i = 0; i < tabl.length; i++) {  //przekatna
-            if (tabl[i][i] == sum2 / 5)
-                sum = sum + tabl[i][i];
+        for (int i = 0; i < tab.length; i++) {
+            if (tab[i][i] == sum2 / 5)
+                sum = sum + tab[i][i];
             else
                 sum = 0;
             if (sum == sum2)
@@ -110,5 +69,6 @@ public class Check {
 
         return false;
     }
+
 
 }
